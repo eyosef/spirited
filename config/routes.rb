dd58c5ab53a35fe9d7eceb3f564b5847183d77e9
Eden <emesfun@outlook.com>
2018-05-23 16:10:34 -0400

    updated the config/routes.rb file to ensure the edit review path posted to Reviews#update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/welcome' => 'welcome#new'
 
+  post '/stores/:store_id/products/:product_id/reviews/:id', to: 'reviews#update'
+
   jsonapi_resources :stores do 
     jsonapi_resources :products do 
       jsonapi_resources :reviews, only: [:index, :show, :new, :create, :edit, :update, :destroy]
